f9dca137508347eceaa0e823f57522aa4bf17bd8
DeclanHurney <declanhurney@gmail.com>
2025-10-08 13:39:57 +0100

    Hardcoded, static graph

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as slt
-
+import plotly.express as pltexp
 slt.title("Weather forecast for the Next (Max 10) Days")
 place = slt.text_input("Place:")
 forecast_days = slt.slider("Forecast Days", min_value=1, max_value=5,
@@ -8,3 +8,10 @@ forecast_days = slt.slider("Forecast Days", min_value=1, max_value=5,
 data_option = slt.selectbox("Select data to view", ("Temperature", "Sky"))
 
 slt.subheader(f"{data_option} for the next {forecast_days} days in {place}")
+
+dates = ["2025-10-09", "2025-10-10", "2025-10-11"]
+temperatures = [10, 11, 15]
+graph = pltexp.line(x=dates, y=temperatures, labels={"x":"Date",
+                                                     "y":"Temperature (C)"})
+slt.plotly_chart(graph)
+
