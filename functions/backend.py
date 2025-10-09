ff624a269b5666c8ab465ac26513eb3ec54ebd11
DeclanHurney <declanhurney@gmail.com>
2025-10-09 15:46:08 +0100

    Final Version

--- functions/backend.py
+++ functions/backend.py
@@ -1,21 +1,43 @@
+from unittest import case
+import streamlit as slt
 import requests as rq
+from dateutil.rrule import weekday
+
 API_KEY = "6e247dcf3b8fd4e3713400b02f504095"
 
-def get_remote_data(place, forecast_days, weather_attribute):
+def get_remote_data(place, forecast_days):
     # See Find API Key below
     # See Find API below
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={
     API_KEY}"
+    # print(url)
     response = rq.get(url)
     data = response.json()
+    # A reading is taken every three hours, 8 readings will be (taken in a day,
+    # that is 40 readings over 5 weeks. Following list will contain 40 entries
+    message = data["message"]
+    if message != "city not found":
+        print(f"Response from API - {message}. Please check {place}")
 
     filtered_data = data["list"]
-
+    # But we only want the no of readings for the no of days entered by
+    # user. Therefore, 8 x number of days will tell us this
+    number_of_readings = 8 * forecast_days
+    # We can slice down the size of the filtered_data to the number we want
+    filtered_data = filtered_data[:number_of_readings]
+    # if weather_attribute is not None and len(weather_attribute) > 0:
+    #     # We can further filter the filtered_data to the kind of weather we want
+    #     match weather_attribute:
+    #         case "Temperature":
+    #             return filtered_data[0]["temp"]
+    #         case "Sky":
+    #             return filtered_data[0]["weather"]
     return filtered_data
 
 if __name__ == "__main__":
-    data = get_remote_data("Tokyo", 1, "Temperature")
-    print(data)
+    data = get_remote_data("Tokyok", 2)
+    # data = data["temp"] / 10
+    print(f"data: {data}")
 
 def get_local_data(days):
     dates_list = ["2025-10-09", "2025-10-10", "2025-10-11"]
--- main.py
+++ main.py
@@ -12,13 +12,36 @@ forecast_days = slt.slider("Forecast Days", min_value=1, max_value=5,
                                                  "forecast")
 data_option = slt.selectbox("Select data to view", ("Temperature", "Sky"))
 
-
-
 slt.subheader(f"{data_option} for the next {forecast_days} days in {place}")
 
-dates, temperatures = bkd.get_local_data(forecast_days)
+if place:
+    # Get the temperature/sky data
+    # dates, temperatures = bkd.get_local_data(forecast_days)
+    try:
+        filtered_data = bkd.get_remote_data(place, forecast_days)
 
-graph = plty_exp.line(x=dates, y=temperatures, labels={"x": "Date",
-                                                     "y":"Temperature (C)"})
-slt.plotly_chart(graph)
+        if data_option == "Temperature":
+            # Create a temperature plot
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            # print(f"temperatures 1: {temperatures}")
+            # # temperature = [images[temperature] for temperature in temperatures]
+            # temperatures = [temperature / 10 for temperature in temperatures]
+            # print(f"temperatures 2: {temperatures}")
+            graph = plty_exp.line(x=dates, y=temperatures, labels={"x": "Date",
+                                                                 "y":"Temperature (C)"})
+            slt.plotly_chart(graph)
+        if data_option == "Sky":
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Clouds":
+                "images/cloud.png","Rain": "images/rain.png", "Snow":
+                "images/snow.png"}
+            sky_images = [images[condition] for condition in sky_conditions]
+            # print(f"sky_conditions: {sky_conditions}")
+            # print(f"sky_images: {sky_images}")
+            # ['Clouds', 'Clouds', 'Clear', 'Clear', 'Clouds', 'Clouds', 'Clouds',
+            #  'Clouds']
+            slt.image(sky_images, width=115)
+    except KeyError:
+        slt.warning(f"No data for this place {place}. Check spelling and try again.")
 
